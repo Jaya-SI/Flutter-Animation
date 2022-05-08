0db3c46250a4b8044eaa359842a1de8dbf2e730c
Jaya-SI <60172081+Jaya-SI@users.noreply.github.com>
2022-05-09 06:52:36 +0800

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:space/pages/category_page.dart';
+import 'package:space/pages/detail_page.dart';
 import 'package:space/pages/home_page.dart';
 import 'package:space/pages/onboarding_page.dart';
 import 'package:space/pages/profile_page.dart';
@@ -28,6 +29,7 @@ class MyApp extends StatelessWidget {
         '/category-page': (context) => CategoryPage(),
         '/whislist-page': (context) => WhisListPage(),
         '/profile-page': (context) => ProfilePage(),
+        '/detail': (context) => DetailPage(),
       },
     );
   }
--- space/space/android/app/src/main/kotlin/com/example/space/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.space
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
